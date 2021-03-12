9c950938489b04bda8733f138ce0e921cef409e5
revuttskaya <revutskaya.ke@gmail.com>
2021-03-13 01:01:35 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
+exports.min = function min(array) {
+    if (array === null || array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let min = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === null || array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === null || array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+};
